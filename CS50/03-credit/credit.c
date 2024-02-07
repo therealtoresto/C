4391f2b04413fc704ebab2d231180e36c1a61092
therealtoresto <therealtoresto@gmail.com>
2024-02-07 13:54:09 +0100

    fixed example

--- CS50/03-credit/credit.c
+++ CS50/03-credit/credit.c
@@ -1,43 +1,75 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <math.h>
 
-int main()
+int main(void)
 {
-    float dollars = -1;
-    while(dollars < 0)
-    {
-        dollars = get_float("Change owed: ");
-        if (dollars == 0.01) return 1;
-    }
-    int coins = round(dollars * 100);
-    int result = 0;
-    while(coins != 0)
-    {   
-        int subCoins = 0;
-        if(coins > 25 || coins == 25)
-        {
-            result = round(coins / 25);
-            coins = coins - result * 25;
-        }
-        if(coins > 10 || coins == 10)
-        {   
-            subCoins = round(coins / 10);
-            result = result + subCoins;
-            coins = coins - subCoins * 10;
-        }
-        if(coins > 5 || coins == 5)
-        {
-            subCoins = round(coins / 5);
-            result = result + round(coins / 5);
-            coins = coins - subCoins * 5;
-        }
-        if(coins > 1 || coins == 1)
-        {
-            result = result + round(coins);
-        }
-        printf("%i\n", result);
-        return 0;
-    }
-}
+    // Prompt the user for a credit card number
+    long long card_number = get_long_long("Number: ");
 
+    // Validate the credit card number using the Luhn algorithm
+    int sum = 0;
+    bool should_double = false;
+    for (long long i = card_number; i > 0; i /= 10)
+    {
+        int digit = i % 10;
+        if (should_double)
+        {
+            digit *= 2;
+            if (digit > 9)
+            {
+                digit = (digit % 10) + (digit / 10);
+            }
+        }
+        sum += digit;
+        should_double = !should_double;
+    }
+    bool is_valid = (sum % 10 == 0);
+
+    // Check the length and starting digits of the credit card number to determine the type of card
+    int length = 0;
+    long long temp = card_number;
+    while (temp > 0)
+    {
+        length++;
+        temp /= 10;
+    }
+    int first_two_digits;
+    int first_digit;
+    if (length == 13)
+    {
+        first_digit = card_number / 10000000000000000;
+    }
+    else if (length == 15)
+    {
+        first_two_digits = card_number / 10000000000000;
+    }
+    else if (length == 16)
+    {
+        first_digit = card_number / 1000000000000000;
+        first_two_digits = card_number / 100000000000000;
+    }
+
+    // Output the result to the user
+    if (!is_valid)
+    {
+        printf("INVALID\n");
+    }
+    else if (length == 15 && (first_two_digits == 34 || first_two_digits == 37))
+    {
+        printf("AMEX\n");
+    }
+    else if (length == 16 && first_two_digits >= 51 && first_two_digits <= 55)
+    {
+        printf("MASTERCARD\n");
+    }
+    else if ((length == 13 || length == 16) && first_digit == 4)
+    {
+        printf("VISA\n");
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
+
+    return 0;
+}
